29c48cdc71d8dfbe90e75b31617782c271a056a3
shamiul <shoq9876@gmail.com>
2016-08-08 17:50:20 +0600

    image changed to file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var route = require('./route.js');
 var fileUpload = require('express-fileupload');
 var app = express();
-app.use(express.static('images'));
+app.use(express.static('files'));
 app.use(fileUpload());
 app.set('view engine', 'ejs');
 app.use('/files', express.static(__dirname + '/files'));
