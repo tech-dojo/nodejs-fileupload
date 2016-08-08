8a6c66e8e771a97d560b4d75ecd91d587ed5fdee
shamiul <shoq9876@gmail.com>
2016-08-08 18:01:48 +0600

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nodejs fileUpload demo
+## step 1 : npm install
+## step 2 : node server.js
+## step 3 : upload a file with dropzone js at localhost:3000/
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var route = require('./route.js');
 var fileUpload = require('express-fileupload');
 var app = express();
-app.use(express.static('files'));
 app.use(fileUpload());
 app.set('view engine', 'ejs');
 app.use('/files', express.static(__dirname + '/files'));
